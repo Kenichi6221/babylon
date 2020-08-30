680b63a2202ef4026c5c898cf42508b1a3c8a4d0
JArdila <J.Ardila1087@gmail.com>
2020-08-30 11:42:33 -0500

    Add srcset images
    Motivation:
    Allow distinct sizes for images, accord to target device screen.

--- babylon_client/src/components/molecule/Section/IntroduceAppSection.js
+++ babylon_client/src/components/molecule/Section/IntroduceAppSection.js
@@ -8,7 +8,16 @@ const IntroduceAppSection = () => {
   return (
     <IntroductionSection>
       <Figure>
-        <Image src="/images/welcomeImage.png" alt="babylon app welcome" />
+        <Image
+          sizes="(max-width: 1800px) 100vw, 1800px"
+          srcSet="/images/welcome/welcomeImage_w_200.png 200w,
+                  /images/welcome/welcomeImage_w_895.png 895w,
+                  /images/welcome/welcomeImage_w_1336.png 1336w,
+                  /images/welcome/welcomeImage_w_1400.png 1400w,
+                  /images/welcome/welcomeImage_w_1800.png 1800w"
+          src="/images/welcome/welcomeImage.png"
+          alt="babylon app welcome"
+        />
         <FigureCaptionIntroduction>
           hello this app is amazing if you want to share your wise
         </FigureCaptionIntroduction>
